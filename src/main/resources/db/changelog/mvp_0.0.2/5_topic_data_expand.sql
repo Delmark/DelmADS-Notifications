e7a47c7266fb7e45aa92553aa9bfcbca2aeec362
Delmark <102899273+Delmark@users.noreply.github.com>
2026-06-30 13:46:35 +0300

    fix: добавил констрейнт в виде триггера на совпадение любых названий и алиасов

--- src/main/resources/db/changelog/mvp_0.0.2/5_topic_data_expand.sql
+++ src/main/resources/db/changelog/mvp_0.0.2/5_topic_data_expand.sql
@@ -6,5 +6,24 @@ alter table notification_topic add column description text;
 alter table notification_topic add column readable_alias varchar(120)
     unique check ( name != readable_alias );
 
+--changeset delmark:alias-trigger-func-definition splitStatements:false
+create or replace function validate_alias() returns trigger as $alias_trg$
+    begin
+        if exists(select 1 from notification_topic where name = new.readable_alias) then
+            raise exception 'alias cannot duplicate any existing topic name';
+        end if;
+        return new;
+    end;
+$alias_trg$ language plpgsql;
+
+--changeset delmark:alias-trigger-create
+create trigger trg_topic_alias_check before insert or update on notification_topic
+    for each row execute function validate_alias();
+
+--changeset delmark:delmark-alias-default-update
+-- таких случаев быть не должно так как алиасы создаются в одном прогоне миграций
+-- однако на всякий случай прогоняю этот скрипт
+update notification_topic set readable_alias = '' where notification_topic.readable_alias is null;
+
 --changeset delmark:set-not-null-alias
 alter table notification_topic alter column readable_alias set not null;
